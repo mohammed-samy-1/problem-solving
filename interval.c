68ed0880e10829160f7cbdbb2cb577eb9bf1c62f
mo samy <medo.samy.0123@mail.com>
2023-08-16 14:16:25 +0300

    interval

---
+++ interval.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//    float num;
+//    scanf("%f", &num);
+//    if (num >= 0 && num <= 25) {
+//        printf("Interval [0,25]");
+//    } else if (num > 25 && num <= 50) {
+//        printf("Interval (25,50]");
+//    } else if (num > 50 && num <= 75) {
+//        printf("Interval (50,75]");
+//    } else if (num > 75 && num <= 100) {
+//        printf("Interval (75,100]");
+//    } else {
+//        printf("Out of Intervals");
+//    }
+//    return 0;
+//}
