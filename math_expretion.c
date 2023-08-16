26dee1b79a326362e38721928ff00f3948db0f73
mo samy <medo.samy.0123@mail.com>
2023-08-16 15:33:10 +0300

    2 intervals

---
+++ math_expretion.c
@@ -0,0 +1,31 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//    int x, y, ans;
+//
+//    char op;
+//    scanf("%i %c %i = %i", &x, &op, &y, &ans);
+//    switch (op) {
+//        case '+':
+//            if (x + y == ans) {
+//                printf("Yes");
+//            } else {
+//                printf("%i", x + y);
+//            }
+//            break;
+//        case '-':
+//            if (x - y == ans) {
+//                printf("Yes");
+//            } else {
+//                printf("%i", x - y);
+//            }
+//            break;
+//        case '*':
+//            if (x * y == ans) {
+//                printf("Yes");
+//            } else {
+//                printf("%i", x * y);
+//            }
+//            break;
+//    }
+//}
---
+++ two_intervals.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//    int x1, y1, x2, y2 ;
+//
+//    char op;
+//    scanf("%i %i %i %i", &x1, &y1, &x2, &y2);
+//    if (x1 >= x2 && x1 <= y2) {
+//        if (y1 < y2) {
+//            printf("%i %i", x1, y1);
+//        } else {
+//            printf("%i %i", x1, y2);
+//        }
+//    }else if (x2 >= x1 && x2 <= y1) {
+//        if (y2 < y1) {
+//            printf("%i %i", x2, y2);
+//        } else {
+//            printf("%i %i", x2, y1);
+//        }
+//    } else printf("-1");
+//    return 0;
+//}
+//
