89369dbdd7ebbcf39015b669dbdb80dbcd3b5388
mo samy <medo.samy.0123@mail.com>
2023-08-16 14:50:39 +0300

    float or int

---
+++ float_or_int.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//
+//int main(void ) {
+//    float num;
+//    scanf("%f", &num);
+//    int toInt = (int) num;
+//    if (num - (float) toInt == 0) {
+//        printf("int %i", toInt);
+//    } else {
+//        printf("float %i %.3f", toInt, (num - (float) toInt));
+//    }
+//}
