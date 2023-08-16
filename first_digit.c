31ed788f8bdd5d5929a7436c0caff8df45daf42e
mo samy <medo.samy.0123@mail.com>
2023-08-16 14:18:41 +0300

    interval

---
+++ first_digit.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//#include <ctype.h>
+//
+//int main(void) {
+//    char s[20];
+//    scanf("%s", &s);
+//    if (!isdigit(s[0]))
+//    {
+//        return 0;
+//    }
+//    if ((s[0] - ('1' - 1)) % 2 == 0 )
+//    {
+//        printf("EVEN");
+//    }
+//    else
+//    {
+//        printf("ODD");
+//    }
+//
+//    return 0;
+//}
---
+++ main.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//
+//int main() {
+//    char ch;
+//    scanf("%c", &ch);
+//    if (ch >= '0' && ch <= '9')
+//    {
+//        printf("IS DIGIT");
+//    }
+//    else if (ch >= 'a' && ch <= 'z')
+//    {
+//        printf("ALPHA\nIS SMALL");
+//    }
+//    else if (ch >= 'A' && ch <= 'Z')
+//    {
+//        printf("ALPHA\nIS CAPITAL");
+//    }
+//    return 0;
+//}
